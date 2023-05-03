300a1ca77ce6df687f4a5ef7915fd718c413d3fd
StanleyTC <rampeustiusken@gmail.com>
2023-05-03 15:23:11 -0300

    first file defined

---
+++ test.py
@@ -0,0 +1,11 @@
+# Comando: pip install opencv-contrib-python
+# TEM QUE TER PIP INSTALADO - INSTALAR PYTHON (pesquisar python na microsoft sotre, instalar e o comando
+# pip estará habilitado)
+
+# duas opções: escrever import cv2 - vamos usar a função VideoCapture, mas se importarmos toda a biblioteca
+# teremos de ficar escrevendo cv2.VideoCapture() toda hora. Para simplificar para apenas escrever 
+# VideoCapture() e não ter que escrever cv2 toda hora, vamos para a segunda opção:
+# from cv2 import VideoCapture
+
+from cv2 import VideoCapture
+
